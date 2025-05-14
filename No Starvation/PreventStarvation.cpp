f1c8d5a85fa919c3c72ed6f306ce98495711857b
wgrzyb271 <w.grzybowski271@gmail.com>
2025-05-14 14:13:27 +0200

    Better GUI

--- No Starvation/PreventStarvation.cpp
+++ No Starvation/PreventStarvation.cpp
@@ -1,141 +1,53 @@
-// zapobieganie zakleszczeniu
+// zapobieganie zaglodzeniu
 
 #include "PreventStarvation.h"
 #include <ncurses.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
-#include <cstdarg>
 #include <random>
 #include <deque>
 #include <iostream>
+#include <cstring>
 
 #define PHILOSOPHERS 5
+#define NO_EATING_TIME 5
 
 sem_t chopsticks[PHILOSOPHERS];
 sem_t screen_lock;
 
 bool running = true;
+bool paused = false;
 bool colors_enabled = false;
 
 enum StatusColor {
-    THINKING,
-    HUNGRY,
+    THINKING = 1,
     EATING,
-    RELEASING,
-    DEFAULT_COLOR
+    HUNGRY,
+    STARVING,
+    HOLDING,
+    CHOPSTICK_FREE,
+    CHOPSTICK_USED
 };
 
 // tablica okreslajaca aktualny stan danego filozofa
 int philosopherState[PHILOSOPHERS];
 // kolejka zawierajaca glodnych filozofow (w stanie HUNGRY)
 std::deque<int> hungerQueue;
+// tablica przechowujaca czas kiedy filozof ostatnio jadl
+time_t last_ate[PHILOSOPHERS];
 // mutex dotyczacy kolejki
 sem_t queueLock;
+sem_t general_lock;
 
+char state[PHILOSOPHERS][4];
+// tablica zjedzonych posilkow
+int meals[PHILOSOPHERS];
+// tablica dostepnych paleczek
+bool chopstick_available[PHILOSOPHERS];
 
-
-// Funkcja generujaca losowa liczbe calkowita z przedzialu [a, b]
-int random_between(int a, int b) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd()); // generator Mersenne Twister
-    std::uniform_int_distribution<> dist(a, b);
-    return dist(gen);
-}
-
-// Funkcja pomocnicza do bezpiecznego wypisywania
-void safe_print(int color, const char* format, ...) {
-    va_list args;
-    sem_wait(&screen_lock);
-
-    if (colors_enabled) {
-        attron(COLOR_PAIR(color));
-    }
-
-    va_start(args, format);
-    vw_printw(stdscr, format, args);
-    va_end(args);
-
-    if (colors_enabled) {
-        attroff(COLOR_PAIR(color));
-    }
-
-    refresh();
-    sem_post(&screen_lock);
-}
-
-// Funkcja filozofa z zapobieganiem zakleszczeniu
-void* prevent_starvation(void* arg) {
-    int id = *(int*)arg;
-    int left = (id + 1) % PHILOSOPHERS;
-    int right = id;
-
-    while (running) {
-        switch (philosopherState[id]) {
-            case THINKING:
-                safe_print(THINKING, "Philosopher %d is thinking\n", id);
-                // filozof mysli - czas wylosowany w sekunach [1, 5] sekund
-                sleep(random_between(2, 5));
-
-                // filozof staje sie glodny
-                philosopherState[id] = HUNGRY;
-
-                // dodanie glodnego filozofa do kolejki
-                sem_wait(&queueLock);
-                if (!isPhilosopherInQueue(id))
-                    hungerQueue.push_back(id);
-                sem_post(&queueLock);
-                break;
-
-            case HUNGRY:
-                // filozof jest glodny - sprawdzamy kolejke
-
-                // zablokowanie "kolejki" zeby miec pewnosc ze nie ulega ona zmianie podczas operowaniu na niej
-                sem_wait(&queueLock);
-
-                // sprawdzenie czy kolejka nie jest pusta i czy jest kolej filozofa o numerze id
-                if (!hungerQueue.empty() && hungerQueue.front() == id) {
-                    // sprawdzenie czy obie paleczki sa wolne
-                    if (sem_trywait(&chopsticks[left]) == 0 && sem_trywait(&chopsticks[right]) == 0) {
-                        safe_print(RELEASING, "Philosopher %d is acquiring left chopstick (%d)\n", id, left);
-                        safe_print(RELEASING, "Philosopher %d is acquiring right chopstick (%d)\n", id, right);
-                        // filozof zaczyna jesc
-                        philosopherState[id] = EATING;
-
-                        // usuniecie filozofa z kolejki
-                        hungerQueue.pop_front();
-                    } else {
-                        // jezeli paleczki nie sa wolne, filozof pozostaje w kolejce
-                        sem_post(&queueLock);
-                        break;
-                    }
-                }
-
-                // odblokowanie kolejki
-                sem_post(&queueLock);
-
-                break;
-
-            case EATING:
-                // filozof je - czas wylosowany w sekunach: [1, 5] sekund
-                safe_print(EATING, "Philosopher %d is eating\n", id);
-                sleep(random_between(1, 4));
-                philosopherState[id] = THINKING;
-
-                // filozof odklada paleczki
-                safe_print(RELEASING, "Philosopher %d is releasing left chopstick (%d)\n", id, left);
-                sem_post(&chopsticks[left]);
-                safe_print(RELEASING, "Philosopher %d is releasing right chopstick (%d)\n", id, right);
-                sem_post(&chopsticks[right]);
-                safe_print(RELEASING, "Philosopher %d released both chopsticks\n", id);
-                break;
-        }
-    }
-    return NULL;
-}
-
-
-
+void draw_interface();
+void pause_barrier();
 
 int main() {
     pthread_t philosophers[PHILOSOPHERS];
@@ -143,37 +55,40 @@ int main() {
 
     initscr();
     cbreak();
-    scrollok(stdscr, TRUE);
     noecho();
     curs_set(FALSE);
+    nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
 
-//    timeout(0);
+    if (has_colors()) {
+        start_color();
+        init_pair(THINKING, COLOR_CYAN, COLOR_BLACK);
+        init_pair(EATING, COLOR_GREEN, COLOR_BLACK);
+        init_pair(STARVING, COLOR_RED, COLOR_BLACK);
+        init_pair(HOLDING, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(CHOPSTICK_FREE, COLOR_WHITE, COLOR_BLACK);
+        init_pair(CHOPSTICK_USED, COLOR_MAGENTA, COLOR_BLACK);
+        colors_enabled = true;
+    } else {
+        colors_enabled = false;
+    }
 
     printw("Starvation Prevention Simulation\n\n");
     printw("Press any key to start Simulation (q or Q to exit)...\n\n");
     int choice = getch();
     if(choice == 'q' or choice == 'Q')
         running = false;
-    clear();
-    nodelay(stdscr, TRUE);
 
-    if (has_colors()) {
-        start_color();
-        init_pair(THINKING, COLOR_CYAN, COLOR_BLACK);
-        init_pair(EATING, COLOR_GREEN, COLOR_BLACK);
-        init_pair(RELEASING, COLOR_YELLOW, COLOR_BLACK);
-        colors_enabled = true;
-    } else {
-        colors_enabled = false;
-        init_pair(DEFAULT_COLOR, COLOR_WHITE, COLOR_BLACK);
-    }
 
     sem_init(&screen_lock, 0, 1);
     sem_init(&queueLock, 0, 1);
+    sem_init(&general_lock, 0, 1);
     for (int i = 0; i < PHILOSOPHERS; ++i) {
         sem_init(&chopsticks[i], 0, 1);
         philosopherState[i] = THINKING;
+        chopstick_available[i] = true;
+        meals[i] = 0;
+        strcpy(state[i], "T");
     }
 
     for (int i = 0; i < PHILOSOPHERS; ++i) {
@@ -182,10 +97,12 @@ int main() {
     }
 
     while (running) {
+        draw_interface();
+
         int ch = getch();
-        if (ch == 'q' || ch == 'Q') {
-            running = false;
-        }
+        if (ch == 'q' || ch == 'Q') running = false;
+        if (ch == ' ') paused = !paused;
+
         usleep(100000); // 0.1s
     }
 
@@ -193,17 +110,89 @@ int main() {
         pthread_join(philosophers[i], NULL);
     }
 
-    sem_destroy(&screen_lock);
-    sem_destroy(&queueLock);
+
     for (int i = 0; i < PHILOSOPHERS; ++i) {
         sem_destroy(&chopsticks[i]);
     }
+
+    sem_destroy(&screen_lock);
+    sem_destroy(&queueLock);
+    sem_destroy(&general_lock);
 
     endwin();
     return 0;
 }
 
+// Funkcja generujaca losowa liczbe calkowita z przedzialu [a, b]
+int random_between(int a, int b) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(a, b);
+    return dist(gen);
+}
 
+// Funkcja filozofa z zapobieganiem zaglodzeniu
+void* prevent_starvation(void* arg) {
+    int id = *(int*)arg;
+    int left = (id + 1) % PHILOSOPHERS;
+    int right = id;
+
+    time_t now = -1;;
+    last_ate[id] = -1;
+
+    while (running) {
+        pause_barrier();
+
+        now = time(NULL);
+
+        if (last_ate[id] > 0 && difftime(now, last_ate[id]) >= NO_EATING_TIME)
+            strcpy(state[id], "S");
+        else {
+            // mysli
+            strcpy(state[id], "T");
+            sleep(random_between(2, 5));
+
+
+            strcpy(state[id], "H");
+        }
+
+        // czekanie na lewa paleczke
+        sem_wait(&chopsticks[left]);
+        chopstick_available[left] = false;
+        strcpy(state[id], "L.");
+
+        pause_barrier();
+
+        // czekanie na prawa paleczke
+        sem_wait(&chopsticks[right]);
+        chopstick_available[right] = false;
+        strcpy(state[id], "LR");
+
+
+        pause_barrier();
+
+        // jedzenie
+        strcpy(state[id], "E");
+        meals[id]++;
+        sleep(random_between(1, 4));
+        last_ate[id] = time(NULL);
+
+        // zwalnianie paleczek
+        sem_post(&chopsticks[left]);
+        chopstick_available[left] = true;
+        strcpy(state[id], ".R");
+
+        pause_barrier();
+
+        sem_post(&chopsticks[right]);
+        chopstick_available[right] = true;
+        strcpy(state[id], "..");
+
+        pause_barrier();
+    }
+
+    return NULL;
+}
 
 
 bool isPhilosopherInQueue(int i){
@@ -211,4 +200,59 @@ bool isPhilosopherInQueue(int i){
         if (current == i) return true;
 
     return false;
+}
+
+void draw_interface() {
+    sem_wait(&screen_lock);
+    erase();
+
+    mvprintw(0, 0, "Dining Philosophers Simulation");
+    mvprintw(1, 0, "Press SPACE to pause/resume, Q to quit.\n");
+
+    for (int i = 0; i < PHILOSOPHERS; i++) {
+        mvprintw(3, i * 18, "Philosopher %d", i);
+        mvprintw(4, i * 18, "Meals: %d", meals[i]);
+
+        if (strcmp(state[i], "E") == 0) attron(COLOR_PAIR(EATING));
+        else if (strcmp(state[i], "T") == 0) attron(COLOR_PAIR(THINKING));
+        else if (strcmp(state[i], "S") == 0) attron(COLOR_PAIR(STARVING));
+        else if (strcmp(state[i], "LR") == 0) attron(COLOR_PAIR(HOLDING));
+        else attron(COLOR_PAIR(HOLDING));
+
+        mvprintw(5, i * 18, "State: %s", state[i]);
+        attroff(COLOR_PAIR(EATING));
+        attroff(COLOR_PAIR(THINKING));
+        attroff(COLOR_PAIR(STARVING));
+        attroff(COLOR_PAIR(HOLDING));
+    }
+
+    mvprintw(8, 0, "Available Chopsticks:");
+    for (int i = 0; i < PHILOSOPHERS; i++) {
+        if (chopstick_available[i])
+            attron(COLOR_PAIR(CHOPSTICK_FREE));
+        else
+            attron(COLOR_PAIR(CHOPSTICK_USED));
+
+        mvprintw(9, i * 12, "[%d] %-3s", i, chopstick_available[i] ? "YES" : "NO");
+        attroff(COLOR_PAIR(CHOPSTICK_FREE));
+        attroff(COLOR_PAIR(CHOPSTICK_USED));
+    }
+
+    mvprintw(11, 0, "Legend:\n");
+    attron(COLOR_PAIR(THINKING)); printw(" T "); attroff(COLOR_PAIR(THINKING)); printw(" - Thinking\n");
+    attron(COLOR_PAIR(EATING)); printw(" E "); attroff(COLOR_PAIR(EATING)); printw(" - Eating\n");
+    attron(COLOR_PAIR(STARVING)); printw(" S "); attroff(COLOR_PAIR(STARVING)); printw(" - Starving\n");
+    attron(COLOR_PAIR(HOLDING)); printw(" L/R "); attroff(COLOR_PAIR(HOLDING)); printw(" - Holding chopstick\n");
+
+    if (paused) mvprintw(17, 0, "-- PAUSED --");
+
+    refresh();
+    sem_post(&screen_lock);
+}
+
+
+void pause_barrier() {
+    while (paused && running) {
+        sleep(2);
+    }
 }
